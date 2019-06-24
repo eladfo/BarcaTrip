f14af59f41104f3aed8abf21ec366c3b712700a7
eladfo <foelad@gmail.com>
2019-06-24 14:35:47 +0300

    Login.html update

--- pages/login/loginController.js
+++ pages/login/loginController.js
@@ -1,59 +1,50 @@
 // login controller
 angular.module("myApp")
 .controller("loginController", function ($scope, $http , $window) {
-    self = this;
-
-
-    /*
-    let serverUrl = "http://localhost:3000/"
-
-    self.login = function()
-    {
-        $http({
-            method : "POST",
-            url : serverUrl + "LogIn",
-            data : {Username:"alonts", Password:"123"}
-        }).then(function success(response){
-            console.log("LogIn successful!")
-        }, function failed(response) {
-            console.log("LogIn failed!")
-        });
-    }
-    */
+   
+   let serverUrl = "http://localhost:3000/"
 
    $scope.LogIn = function() 
    {
-       $scope.Username = "alonts"; //need to change to the real name of the user!
-       $window.sessionStorage.setItem("Username","alonts");
-       document.getElementById("MenuFavorite").style = "visibility: visible;";
-       document.getElementById("MenuHome").style = "visibility: visible;"
-       document.getElementById("MenuLogIn").style = "visibility: hidden;"
-       document.getElementById("MenuRegister").style = "visibility: hidden;"
-       
-       document.getElementById("LogOutbut").style = "visibility: visible;"
-       document.getElementById("LogOutlbl").innerHTML = "Hello " + $scope.Username;
+        let userJsn = {
+            "Username":$scope.username,
+            "Password":$scope.password
+        };
 
-
-       $window.sessionStorage.setItem("Token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsb250cyIsImlhdCI6MTU2MDg2ODc5MSwiZXhwIjoxNTYwOTU1MTkxfQ.gLs0t03HPa9b8FFT48jv5-bfbDrmvQ2Hx2TtntvZ7J8");
-       $scope.UpdateNumOfFavorite();
-       $window.location.href = '#!main'
+        $http({
+            method: 'POST',
+            url: serverUrl + "login",
+            data: userJsn
+        }).then(function (success) {
+            $scope.token = success.data;
+            document.getElementById("MenuFavorite").style = "visibility: visible;";
+            document.getElementById("MenuHome").style = "visibility: visible;"
+            document.getElementById("MenuLogIn").style = "visibility: hidden;"
+            document.getElementById("MenuRegister").style = "visibility: hidden;"
+            document.getElementById("LogOutbut").style = "visibility: visible;"
+            document.getElementById("LogOutlbl").innerHTML = "Hello " + $scope.username;
+            $window.sessionStorage.setItem("Token", $scope.token);
+            $scope.UpdateNumOfFavorite(userJsn.Username);
+            $window.location.href = '#!main'
+        }, function (error) {
+            alert("One of the details you entered are incorrect.");
+        });      
    }
 
-    $scope.UpdateNumOfFavorite = function()
+    $scope.UpdateNumOfFavorite = function(Username)
     {
         $http({method : "GET",
-        url : "http://localhost:3000/private/getFavoritesCount/"+$window.sessionStorage.getItem("Username") , 
+        url : "http://localhost:3000/private/getFavoritesCount/"+$window.sessionStorage.getItem(Username) , 
         headers : {
         "Authorization" : $window.sessionStorage.getItem("Token")
         }
         }).then(function mySuccess(response) {
-        document.getElementById("numbernoti").innerHTML = response.data.length;
+            document.getElementById("numbernoti").innerHTML = response.data.length;
         } ,
         function myError(response) {
         });
     }
 });
 
-///http://127.0.0.1:5500/index.html#!/
 
 
