99ab39f4707b8fd4283d394263b3e137c68585e7
eladfo <foelad@gmail.com>
2019-07-07 14:55:51 +0300

    changes within about.html plus register.html

--- pages/about/aboutController.js
+++ pages/about/aboutController.js
@@ -1,15 +1,15 @@
 // about controller
 angular.module("myApp")
 .controller("aboutController", function ($scope) {
-     $scope.slideIndex = 1;
-     var i;
-     var x = document.getElementsByClassName("mySlides");
-     if (1 > x.length) {$scope.slideIndex = 1}
-     if (1 < 1) {$scope.slideIndex = x.length}
-     for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-     }
-     x[$scope.slideIndex-1].style.display = "block";  
+   //   $scope.slideIndex = 1;
+   //   var i;
+   //   var x = document.getElementsByClassName("mySlides");
+   //   if (1 > x.length) {$scope.slideIndex = 1}
+   //   if (1 < 1) {$scope.slideIndex = x.length}
+   //   for (i = 0; i < x.length; i++) {
+   //      x[i].style.display = "none";  
+   //   }
+   //   x[$scope.slideIndex-1].style.display = "block";  
 
 
  $scope.plusDivs = function(n) {
--- pages/login/loginController.js
+++ pages/login/loginController.js
@@ -2,67 +2,60 @@
 angular.module("myApp")
 .controller("loginController", function ($scope, $http , $window) {
 
+    let serverUrl = "http://localhost:3000/"
 
-
-    $http({method : "GET",
-    url : "http://localhost:3000/getRandomPOI/0.5"
+    $http({
+        method : "GET",
+        url : "http://localhost:3000/getRandomPOI/0.5"
     }).then(function mySuccess(response)
     {
         $scope.favorite = new Array();
         $scope.favorite[0] ={Name: response.data.res1.Name , Picture: response.data.res1.Picture };
         $scope.favorite[1] ={Name: response.data.res2.Name , Picture: response.data.res2.Picture };
         $scope.favorite[2] ={Name: response.data.res3.Name , Picture: response.data.res3.Picture };
-
-
     } ,
     function myError(response) {
         alert(response.data) 
     });
 
-
-
-   let serverUrl = "http://localhost:3000/"
-  
-
-
-   $scope.ImgClick = function(pic)
-   {    
-       $scope.Res = pic.IsFavorite;
-       if($scope.isConecting)
-         $scope.updateFavorite();
-
-         $http.get('http://localhost:3000/getPOIByName/'+pic.Name).then(function(response){
-         $scope.id = response.data[0].ID ;      
-         $scope.title = response.data[0].Name ; 
-         $scope.users = response.data[0].Num_of_Users ; 
-         $scope.desc = response.data[0].Description ;
-         $scope.img = response.data[0].Picture ; 
-         $scope.rank = response.data[0].Rank ;
-                
-         $scope.tableVis = false;        
-         $http.get('http://localhost:3000/increaseWatchPOI/'+$scope.id).then(function(response){
-
-            $http.get('http://localhost:3000/getLastReviews/'+$scope.id).then(function(response){
-            $scope.reviews = new Array();
-            for(i=0 ; i<Object.keys(response.data).length ; i++)
-            {
-                $scope.reviews[i]   =  { Date:response.data[i].Date, Review: response.data[i].Review ,Rank: response.data[i].Rank };
-            }
-                  
-            if(Object.keys(response.data).length != 0)
-                $scope.tableVis = true;  
-            });
-            
-         });
-         document.getElementById('id01').style.display='block'
-    });
     
-   }
+    $scope.ImgClick = function(pic)
+    {    
+        $scope.Res = pic.IsFavorite;
+        if($scope.isConecting)
+            $scope.updateFavorite();
+
+        $http.get('http://localhost:3000/getPOIByName/'+pic.Name).then(function(response)
+        {
+            $scope.id = response.data[0].ID ;      
+            $scope.title = response.data[0].Name ; 
+            $scope.users = response.data[0].Num_of_Users ; 
+            $scope.desc = response.data[0].Description ;
+            $scope.img = response.data[0].Picture ; 
+            $scope.rank = response.data[0].Rank ;               
+            $scope.tableVis = false;     
+               
+            $http.get('http://localhost:3000/increaseWatchPOI/'+$scope.id).then(function(response)
+            {
+                $http.get('http://localhost:3000/getLastReviews/'+$scope.id).then(function(response)
+                {
+                    $scope.reviews = new Array();
+                    for(i=0 ; i<Object.keys(response.data).length ; i++)
+                    {
+                        $scope.reviews[i] = {Date:response.data[i].Date, Review: response.data[i].Review ,Rank: response.data[i].Rank };
+                    }             
+                    if(Object.keys(response.data).length != 0)
+                        $scope.tableVis = true;  
+                });  
+            });
+            document.getElementById('id01').style.display='block'
+        });
+    }
 
 
 
-   $scope.LogIn = function() 
-   {
+    $scope.LogIn = function() 
+    {
         let userJsn = {
             "Username":$scope.username,
             "Password":$scope.password
@@ -87,7 +80,7 @@ angular.module("myApp")
         }, function (error) {
             alert(error.data.err);
         });      
-   }
+    }
 
     $scope.UpdateNumOfFavorite = function()
     {
@@ -102,8 +95,6 @@ angular.module("myApp")
         function myError(response) {
         });
     }
-
-
 });
 
 
